615f42b0680eaeb7dbd095e48192c54ab7f25504
huguet10 <huguet10@gmail.com>
2016-05-12 19:18:20 +0200

    Subida inicial

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path=require('path');
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js*/,
+				exclude: /node_modules/,
+				loaders: ['react-hot', 'babel']
+			}
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist')
+	}
+}
